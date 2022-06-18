d1a91b787c1966698a68e23aeb275b509a2f896e
Arnau <arnau@Arnaus-MacBook-Pro.local>
2022-06-18 16:06:45 +0200

    refactor/ fetch the data directly from the backend and server config improvements

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import fetch from "node-fetch";
 
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+
+app.get("/characters", (req, res) => {
+    fetch('https://rickandmortyapi.com/api/character')
+        .then(response => response.json())
+        .then(data => {
+            res.json(data.results);
+        })
 });
 
-app.listen(3001, () => {
-    console.log(`Rick and Morty are listening on port ${port}`);
+app.listen(5000, () => {
+  console.log(`Rick and Morty are listening on port ${port}`);
 });
