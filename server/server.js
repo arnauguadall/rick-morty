1677fb309ed43f5228abbb73e571fef5016cf767
Arnau <arnau@Arnaus-MacBook-Pro.local>
2022-06-18 21:30:58 +0200

    fix/ params requests to use the id sent from the frontend

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
-import express from 'express';
+import express from "express";
 import fetch from "node-fetch";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-
 app.get("/characters", (req, res) => {
-    fetch('https://rickandmortyapi.com/api/character')
-        .then(response => response.json())
-        .then(data => {
-            res.json(data.results);
-        })
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data.results);
+    });
+});
+
+app.get("/character/:id", (req, res) => {
+  fetch(`https://rickandmortyapi.com/api/character/${req.params.id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    });
 });
 
 app.listen(5000, () => {
