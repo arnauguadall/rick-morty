a1c337a747baebefb1cd64196ee3530743057912
Arnau <arnau@Arnaus-MacBook-Pro.local>
2022-06-19 23:26:29 +0200

    refactor/ characterdetail UI

--- client/src/views/characterDetail/characterDetail.js
+++ client/src/views/characterDetail/characterDetail.js
@@ -8,23 +8,25 @@ import "./characterDetail.css";
 const CharacterDetail = () => {
   let { id } = useParams();
   const [character, setCharacter] = useState({});
+  const [characterLocation, setCharacterLocation] = useState({});
 
   const { name, status, image, species, location, episode } = character;
+
+  console.log(character);
 
   useEffect(() => {
     if (id) {
       fetch(`/character/${id}`)
         .then((response) => response.json())
         .then((data) => {
-          setCharacter(data);
+          setCharacter(data);       
         });
+
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const fav = character.fav ? "#D5D803" : "#FFFFFF";
-
-  
 
   return id ? (
     <div className="characterDetail__position">
@@ -33,23 +35,27 @@ const CharacterDetail = () => {
           <img src={image} alt={`${name} character`} />
         </div>
         <div className="characterDetail__info__column">
-          <div className="characterDetail__svg__fav">
-            <Star style={{ width: "44px", height: "44px", fill: fav }} />
-          </div>
           <h2>{name}</h2>
 
-          <div className="characterDetail__status__align">
+          <div className="characterDetail__row__align">
             <span className={`characterDetail__status__${status}`} /> {status} -{" "}
             {species}
           </div>
-          <div className="characterDetail__status__align">
+          <div className="characterDetail__row__align">
             Origin: {origin?.name || "Unknown"}
           </div>
-          <div className="characterDetail__status__align">
+          <div className="characterDetail__row__align">
             Location: {location?.name || "Unknown"}
           </div>
 
           <div>Seen in {episode?.length} episodes</div>
+        </div>
+        <div className="characterDetail__planet__column">
+          <div className="characterDetail__svg__fav">
+            <Star style={{ width: "44px", height: "44px", fill: fav }} />
+          </div>
+
+          <div className="characterDetail__row__align"> Click the star to save to favs</div>
         </div>
       </div>
     </div>
