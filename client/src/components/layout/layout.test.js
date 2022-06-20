0686250f91a894a57575eb033eb511c605067d1a
Arnau <arnau@Arnaus-MacBook-Pro.local>
2022-06-20 21:19:57 +0200

    refactor/ final test suite for layout

--- client/src/components/layout/layout.test.js
+++ client/src/components/layout/layout.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Layout from "./layout.js";
+import { MemoryRouter } from "react-router-dom"; // our router
 
 // Jest mock to use react-router-dom in this component
 const mockedUsedNavigate = jest.fn();
@@ -12,16 +13,19 @@ describe("<Layout /> component", () => {
   let component;
 
   beforeEach(() => {
-    component = render(<Layout />);
+    component = render(
+      <MemoryRouter>
+        <Layout />
+      </MemoryRouter>
+    );
   });
 
   it("should render the <Layout /> component properly", () => {
     expect(component.container).toBeInTheDocument();
   });
 
-  test("should render the 404 image", () => {
-    const logo = screen.getByRole("img");
-    expect(logo).toHaveAttribute("src", "404.png");
-    expect(logo).toHaveAttribute("alt", "404 page");
+  test("should render two option menus available", () => {
+    expect(screen.getByText(/Back to characters/i)).toBeInTheDocument();
+    expect(screen.getByText(/Create user/i)).toBeInTheDocument();
   });
 });
